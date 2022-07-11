8f4adac5f592d7258f92b10a4992074dde3420be
Kirill Romanov <freibird22@gmail.com>
2022-07-11 17:01:42 +0300

    Task_1 added

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
+//a = 5; b = 7 -> max = 7 
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int numberA = new Random().Next(-100, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(-100, 100);
+Console.WriteLine(numberB);
+int max = numberA;
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+Console.Write("max= ");
+Console.WriteLine(max);
