c27dedccdfb14fa6049ccad5b8033b91ec9893e8
Kirill Romanov <freibird22@gmail.com>
2022-07-15 12:42:37 +0300

    Find second digit in three-digit number.

---
+++ Task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+
+int result = FindSecondNumber(a);
+Console.WriteLine(result);
+
+int FindSecondNumber(int number)
+{
+    int secondDigit = number % 100 / 10;
+    return secondDigit;
+}
