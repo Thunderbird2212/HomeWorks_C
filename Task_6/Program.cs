f7dbfd78668f469e2da72eaa92d6fa43adcaba26
Kirill Romanov <freibird22@gmail.com>
2022-07-15 18:40:21 +0300

    Third digit.

---
+++ Task_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine("Enter your number ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99)
+{
+Console.WriteLine((number / 100) % 10);
+}
+else
+{
+Console.WriteLine("No third digit");
+}
+
+
+
+
+
