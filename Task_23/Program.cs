a8f4bd7b2b132fcaefff2f97bfed89fa4427badc
Kirill Romanov <freibird22@gmail.com>
2022-07-20 17:25:07 +0300

    Find Cubes.

---
+++ Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Enter your number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Table of Cubes ");
+
+for (int i = 1; i < number + 1; i++)
+{
+    int TableCubes = i * i * i;
+    Console.Write($"{" "} {TableCubes}");
+}
+Console.WriteLine();
